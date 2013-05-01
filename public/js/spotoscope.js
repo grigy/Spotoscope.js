140d2e1540100d298440be5d0985e695fcad3bf3
Grigor Yeghiazaryan <yeghiazaryan@gmail.com>
2013-05-01 11:50:56 +0400

    router test

--- public/js/spotoscope.js
+++ public/js/spotoscope.js
@@ -3,6 +3,15 @@ $(function() {
   Parse.initialize("81nQtGIwTXAzDf6g4UfUsOZOzNd9DJ4ViDCwEw9Q", 
                    "uaGrVXt1LKQ2Meck9HmvIGKxczPjFAYf78vgLkrJ");
 
-
+  var Router = Backbone.Router.extend({
+    routes: {
+      '': 'home'
+    }
+  });
+  var router = Router();
+  router.on('route:home', function() {
+    console.log('Home page');
+  });
+  Backbone.history.start();
 
 });
